d844ae7b6982ac49b7d261e2c3203a5cf977455d
Shreya <shreyaray.1206@gmail.com>
2023-12-26 13:33:38 +0530

    firstCommit

---
+++ basic.py
@@ -0,0 +1,12 @@
+x = str(3) # x will be '3'
+y = int(3) # y will be 3
+z = float(3) # 
+
+print(x);
+print(y);
+print(z);
+
+x =5;
+y = str(9);
+print(type(x));
+print(type(y));
---
+++ prac.py
@@ -0,0 +1,5 @@
+x= 9
+print(x);
+
+if 5 > 2:
+  print("five is greater than two!")
